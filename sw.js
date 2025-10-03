40485ce72cfbd0a1b3a6441691514a9f3bee1e11
babaluu2025 <poratfaktura@gmail.com>
2025-10-03 20:19:16 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,5 +2,5 @@ self.addEventListener('install', e => {
   console.log("Service Worker installed");
 });
 self.addEventListener('fetch', e => {
-  // Trenutno samo propušta sve dalje
+  // default - propušta sve
 });
