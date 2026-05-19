5235f82a5f8c19dc638324e91d38eecf06850896
babaluu2025 <poratfaktura@gmail.com>
2026-05-19 20:17:16 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,73 @@
-self.addEventListener('install', e => {
-  console.log("Service Worker installed");
+const CACHE_NAME = 'konobari-v1';
+
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+/* INSTALL */
+self.addEventListener('install', (event) => {
+
+  self.skipWaiting();
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(FILES_TO_CACHE);
+      })
+
+  );
+
 });
-self.addEventListener('fetch', e => {
-  // default - propušta sve
+
+/* ACTIVATE */
+self.addEventListener('activate', (event) => {
+
+  event.waitUntil(
+
+    caches.keys().then((keys) => {
+
+      return Promise.all(
+
+        keys.map((key) => {
+
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+
+        })
+
+      );
+
+    })
+
+  );
+
+  self.clients.claim();
+
+});
+
+/* FETCH */
+self.addEventListener('fetch', (event) => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+      .then((response) => {
+
+        return response || fetch(event.request);
+
+      })
+      .catch(() => {
+
+        return caches.match('./index.html');
+
+      })
+
+  );
+
 });
